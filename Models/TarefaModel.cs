beb574a2a94410dc1b6f475dc8645e06a923952a
FernandoGarciaRangel <fernandolyon.fg@gmail.com>
2025-03-30 19:29:49 -0300

    att

---
+++ Models/TarefaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EstudoApiTarefas.Models;
+
+public class TarefaModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    
+    
+}
---
+++ Routes/TarefaRoute.cs
@@ -0,0 +1,9 @@
+﻿namespace EstudoApiTarefas.Routes;
+
+public static class TarefaRoute
+{
+    public static void TarefaRoutes(this WebApplication app)
+    {
+        app.MapGet("tarefa", () => "Olá pessoa!");
+    }
+}

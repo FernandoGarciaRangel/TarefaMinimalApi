00cd2431466771ac561ed8f81b48ad28f4fa2120
FernandoGarciaRangel <fernandolyon.fg@gmail.com>
2025-03-30 21:55:32 -0300

    Criando models

--- Models/TarefaModel.cs
+++ Models/TarefaModel.cs
@@ -2,8 +2,14 @@
 
 public class TarefaModel
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public TarefaModel(string name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+    
+    public Guid Id { get; init; }  //init significa que só pode ser alterado uma vez
+    public string Name { get; private set; } = string.Empty;
     
     
 }
--- Routes/TarefaRoute.cs
+++ Routes/TarefaRoute.cs
@@ -1,9 +1,11 @@
-﻿namespace EstudoApiTarefas.Routes;
+﻿using EstudoApiTarefas.Models;
+
+namespace EstudoApiTarefas.Routes;
 
 public static class TarefaRoute
 {
     public static void TarefaRoutes(this WebApplication app)
     {
-        app.MapGet("tarefa", () => "Olá pessoa!");
+        app.MapGet("tarefa", () => new TarefaModel());
     }
 }
